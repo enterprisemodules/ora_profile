8424693f71a3793e32292aa08524180723c9cd3c
Colby Goettel <colby.goettel@gmail.com>
2019-01-01 13:55:16 -0600

    style changes, comment out unknown resource type

--- manifests/database.pp
+++ manifests/database.pp
@@ -12,19 +12,19 @@
 #
 # @summary This is a highly customizable Puppet profile class to define an Oracle database on your system.
 # In it's core just adding:
-# 
+#
 # ```
 # contain ora_profile::database
 # ```
-# 
-# Is enough to get an Oracle 12.2 database running on your system. 
-# 
+#
+# Is enough to get an Oracle 12.2 database running on your system.
+#
 # But sometimes you have specific uses cases that are not handled well by the standard classes. This profile class allows you to add your own code to the execution.
-# 
+#
 # ## Stages
-# 
+#
 # Defining and starting an Oracle database on you system goes through several stages(These are not puppet stages):
-# 
+#
 # - `sysctl`           (Set all required sysctl parameters)
 # - `limits`           (Set all required OS limits)
 # - `packages`         (Install all required packages)
@@ -42,47 +42,47 @@
 # - `db_profiles`      (Define all required Oracle profiles)
 # - `db_users`         (Define all required Oracle users)
 # - `db_startup`       (Make sure the database restarts after a reboot)
-# 
-# All these stages have a default implementation. This implementation is suitable to get started with. These classed all have parameters you can customize through hiera values. The defaults are specified in the module's `data/default.yaml` file. 
-# 
+#
+# All these stages have a default implementation. This implementation is suitable to get started with. These classes all have parameters you can customize through hiera values. The defaults are specified in the module's `data/default.yaml` file.
+#
 # ## before classes
-# 
-# But sometimes this is not enough and you would like to add some extra definitions, you can, for example, add a Puppet class to be executed after the `systctl` stage is done and before the `limits` is done. You can do this by adding the next line to your yaml data:
-# 
+#
+# Sometimes this is not enough and you would like to add some extra definitions. You can. For example, adding a Puppet class to be executed after the `systctl` stage is done and before the `limits` is done. You can do this by adding the following line to your yaml data:
+#
 # ```yaml
-# ora_profile::database::before_sysctl:   my_profile::my_extra_class
+# ora_profile::database::before_sysctl: my_profile::my_extra_class
 # ```
-# 
+#
 # ## after classes
-# 
+#
 # You can do the same when you want to add code after one of the stage classes:
-# 
+#
 # ```yaml
-# ora_profile::database::after_sysctl:   my_profile::my_extra_class
+# ora_profile::database::after_sysctl: my_profile::my_extra_class
 # ```
-# 
+#
 # ## Skipping
-# 
-# Sometimes organisation use different modules and mechanisms to implement a feature and you want to skip the class:
-# 
+#
+# Sometimes, organizations use different modules and mechanisms to implement a feature and you may want to skip a class:
+#
 # ```yaml
-# ora_profile::database::sysctl:   skip
+# ora_profile::database::sysctl: skip
 # ```
-# 
+#
 # ## Replacing
-# 
+#
 # Or provide your own implementation:
-# 
+#
 # ```yaml
-# ora_profile::database::sysctl:   my_profile::my_own_implementation
+# ora_profile::database::sysctl: my_profile::my_own_implementation
 # ```
-# 
-# This mechanism can be used for all named stages and makes it easy to move from an easy setup with a running standard database to a fully customized setup using a lot of your own classes plugged in.
-# 
+#
+# This mechanism can be used for all named stages and makes it easy to move from an easy setup with a running standard database to a fully-customized setup using a lot of your own classes plugged in.
+#
 # Look at the description of the stages and their properties.
-# 
+#
 # At this level you can also customize some generic settings. Check the settings for:
-# 
+#
 # - `version`
 # - `dbname`
 # - `os_user`
@@ -91,17 +91,15 @@
 # - `source`
 # - `oracle_base`
 # - `oracle_home`
-# 
+#
 # Here is an example on how you can do this:
-# 
+#
 # ```puppet
-# 
-# class {'ora_profile::database':
+# class { 'ora_profile::database':
 #   dbname  => 'EM',
 #   source  => 'http://www.example.com/database_files',
 #   version => '11.2.0.3',
 # }
-# 
 # ```
 #
 # @param [Enum['11.2.0.1',
