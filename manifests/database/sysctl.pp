62cfb10b062ea6fcaf699ae48386402bd920a94c
Colby Goettel <colby.goettel@gmail.com>
2019-01-01 14:09:28 -0600

    uncommented offending line

--- manifests/database/sysctl.pp
+++ manifests/database/sysctl.pp
@@ -45,11 +45,11 @@ class ora_profile::database::sysctl(
   Hash $list,
 ) inherits ora_profile::database {
 
-  # if $list.keys.size > 0 {
-  #   echo { "Ensure Sysctl param(s) ${list.keys.join(',')}":
-  #     withpath => false,
-  #   }
-  # }
+  if $list.keys.size > 0 {
+    echo { "Ensure Sysctl param(s) ${list.keys.join(',')}":
+      withpath => false,
+    }
+  }
 
   $defaults = {
     ensure  => 'present',
