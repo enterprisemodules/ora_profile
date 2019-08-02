9ae20d05fc0464cda541d4f0f313f1a4617c1717
Remy van Berkum <rvanberkum@bol.com>
2019-08-02 16:14:39 +0200

    [afd_label] Required to set ORACLE_BASE in 19c
    Change-Id: I77b61f32797234b8d10f7754e8bc4e6b1365992f

--- manifests/database/asm_storage/afd_label.pp
+++ manifests/database/asm_storage/afd_label.pp
@@ -8,11 +8,12 @@ class ora_profile::database::asm_storage::afd_label()
 {
   $grid_home = lookup('ora_profile::database::grid_home')
   $grid_user = lookup('ora_profile::database::grid_user')
+  $grid_base = lookup('ora_profile::database::grid_base')
 
   $ora_profile::database::asm_storage::disk_devices.each |$device, $values| {
     exec { "add afd label ${values['label']} to device /dev/${device}":
       command     => "${grid_home}/bin/asmcmd afd_label ${values['label']} /dev/${device}",
-      environment => ["ORACLE_HOME=${grid_home}"],
+      environment => ["ORACLE_HOME=${grid_home}","ORACLE_BASE=${grid_base}"],
       user        => $grid_user,
       path        => "/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:${grid_home}/bin",
       unless      => "${grid_home}/bin/asmcmd afd_lslbl /dev/${device} | grep '^${values['label']} .* /dev/${device}$'",
