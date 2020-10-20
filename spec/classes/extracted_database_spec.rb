030153b0bca48b59971d1f5f731953aa186574a3
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-10-20 13:36:14 +0200

    [extracted_database] Add required parameters to class

---
+++ spec/classes/extracted_database_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'ora_profile::extracted_database' do
+
+  on_supported_os(:facterversion => '2.4').each do |os, os_facts|
+    context "on #{os}" do
+      next if os =~ /windows/
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
