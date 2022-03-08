a55715ae55bac89bdb5e580f46880ee37a06d4aa
Bert Hajee <bert.hajee@enterprisemodules.com>
2022-03-08 17:11:18 +0100

    [em_license] Use license::activate instead of license::available

--- manifests/database/em_license.pp
+++ manifests/database/em_license.pp
@@ -8,11 +8,5 @@
 # See the file "LICENSE" for the full license governing this code.
 #
 class ora_profile::database::em_license {
-  include stdlib
-
-  unless defined(Class['easy_type::license::available']) {
-    class { 'easy_type::license::available':
-      stage => 'setup',
-    }
-  }
+  contain easy_type::license::activate
 }
